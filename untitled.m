d49cb56afe09ff3cf8b12dc42e9faf93a2f861fa
eltuzzador <spiderhp99@gmail.com>
2024-03-15 20:48:24 +0100

    Aggiunto contorno

--- untitled.m
+++ untitled.m
@@ -17,11 +17,32 @@ eroded_image_medfilt = medfilt2(eroded_image, [5, 5]);
 grey_level = graythresh(eroded_image_medfilt);
 binarized_image = imbinarize(eroded_image_medfilt, grey_level);
 
-%bware = bwareaopen(tumore, 100);
+
 image_without_results = bwareaopen(binarized_image, 150);
 
-results = binarized_image - image_without_results;
+occurences = binarized_image - image_without_results;
 
-Helpers.Subplot({binarized_image, image_without_results, results})
+%Helpers.Subplot({binarized_image, image_without_results, occurences})
+ 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%fill = imfill(tumore, 'holes');
+% Etichetta le regioni connesse nell'immagine binarizzata
+[label_matrix, num_labels] = bwlabel(occurences);
+
+% Ottiene le proprietà delle regioni connesse
+region_props = regionprops(label_matrix, 'Area', 'BoundingBox', 'ConvexHull');
+Helpers.Subplot({input_image})
+hold on; % Abilita la sovrapposizione dei tracciati
+
+% Itera su tutte le regioni connesse trovate
+for i = 1:num_labels
+    % Disegna il contorno convesso (convex hull) della regione
+    plot(region_props(i).ConvexHull(:,1), region_props(i).ConvexHull(:,2), 'r', 'LineWidth', 2);
+    
+    % Mostra l'area della regione
+    text(region_props(i).BoundingBox(1), region_props(i).BoundingBox(2) - 10, ...
+        ['Area: ' num2str(regioni_prop(i).Area)], 'Color', 'yellow', 'FontSize', 10);
+end
+
+hold off;% Disbilita la sovrapposizione dei tracciati
+s=1;
