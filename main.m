81e8aa1851d5ce610e453b2c1723a4a32ce2b09a
eltuzzador <spiderhp99@gmail.com>
2024-03-16 18:04:44 +0100

    TODO

--- main.m
+++ main.m
@@ -1,10 +1,12 @@
 inputImage  = imread('imagesProject\tumore1.png');
+%inputImage  = imread('imagesProject\2tumori.png');
 % Definisci l'elemento strutturale (diamante) con un raggio di 2
-structureElement = strel('diamond', 4);
+structureElement = strel('diamond', 2);
+
 
 % Erosione dell'immagine con l'elbinarizedImageemento strutturale definito
 erodedImage = imerode(inputImage, structureElement);
-
+%Helpers.imsshow({inputImage, erodedImage});
 % Converte l'immagine in scala di grigi se necessario
 if size(erodedImage, 3) == 3
     erodedImage = rgb2gray(erodedImage);
@@ -16,27 +18,65 @@ erodedImageMedfilt = medfilt2(erodedImage, [5, 5]);
 
 binarizedImage = Helpers.otsubin(erodedImageMedfilt);
 
-image_without_results = bwareaopen(binarizedImage, 150);
+Helpers.imsshow({inputImage,binarizedImage})
+imageWithoutResults = bwareaopen(binarizedImage, 150);
 
-occurences = binarizedImage - image_without_results;
+occurences = binarizedImage - imageWithoutResults;
 
 % Etichetta le regioni connesse nell'immagine binarizzata
 [label_matrix, num_labels] = bwlabel(occurences);
 
+
+centroids = regionprops(label_matrix, 'Centroid');
 % Ottiene le proprietà delle regioni connesse
-regionProps = regionprops(label_matrix, 'Area', 'BoundingBox', 'ConvexHull');
-Helpers.imsshow({inputImage})
+% TODO: controllare utilità convexHull
+regionProps = regionprops(label_matrix, 'Area', 'Perimeter', 'BoundingBox', 'ConvexHull');
+
+resultImageBin = Helpers.otsubin(inputImage);
+imshow(resultImageBin);
+ 
+% Crea una maschera della regione di interesse
+maschera_regione = poly2mask(centroids(1).Centroid(1), centroids(1).Centroid(2), size(resultImageBin, 1), size(resultImageBin, 2));
+
+% Trova il perimetro della regione di interesse
+%perimetro_regione = bwperim(maschera_regione);
+
+% Visualizza l'immagine originale con il perimetro della regione di interesse evidenziato
+
+%hold on; 
+%plot(centroids(1).Centroid(1), centroids(1).Centroid(2), 'r.', 'MarkerSize', 20);  % Punto rosso con dimensione 20
+%
+%%visboundaries(perimetro_regione, 'Color', 'r', 'LineWidth', 1.5);
+%hold off;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%img=resultImageBin - binarizedImage
+
+Helpers.imsshow(resultImageBin);
 hold on; % Abilita la sovrapposizione dei tracciati
 
 % Itera su tutte le regioni connesse trovate
 for i = 1:num_labels
     % Disegna il contorno convesso (convex hull) della regione
     plot(regionProps(i).ConvexHull(:,1), regionProps(i).ConvexHull(:,2), 'r', 'LineWidth', 2);
-    
-    % Mostra l'area della regione
-    text(regionProps(i).BoundingBox(1), regionProps(i).BoundingBox(2) - 10, ...
-        ['Area: ' num2str(regionProps(i).Area)], 'Color', 'yellow', 'FontSize', 10);
 
+    % Mostra l'area della regione
+    text(regionProps(i).BoundingBox(1), regionProps(i).BoundingBox(2) - 25, ...
+        ['Area: ' num2str(regionProps(i).Area)], 'Color', 'yellow', 'FontSize', 10);
+text(regionProps(i).BoundingBox(2)', regionProps(i).BoundingBox(1), ...
+        ['Perimeter: ' num2str(regionProps(i).Perimeter)], 'Color', 'yellow', 'FontSize', 10);
 end
 
 hold off;% Disbilita la sovrapposizione dei tracciati
