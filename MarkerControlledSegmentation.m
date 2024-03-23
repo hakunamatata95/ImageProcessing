5e6acb95601f76d2b9c0a593ed96ebdc3e4c51c5
Simone Carmine CIAVARELLA <simone.ciavarella@alten.it>
2024-03-23 11:42:39 +0100

    Added segmentation

--- MarkerControlledSegmentation.m
+++ MarkerControlledSegmentation.m
@@ -26,13 +26,13 @@ sobel_x = [-1 0 1; -2 0 2; -1 0 1]; % Sobel kernel per Gx
 sobel_y = [-1 -2 -1; 0 0 0; 1 2 1]; % Sobel kernel per Gy
 
 % Applicazione della convoluzione con i kernel Sobel
-%gradiente_x = imfilter(double(img_resized), sobel_x);
-%gradiente_y = imfilter(double(img_resized), sobel_y);
+gradiente_x = imfilter(double(img_resized), sobel_x);
+gradiente_y = imfilter(double(img_resized), sobel_y);
 %gradiente_x = conv2(double(img), sobel_x, 'same');
 %gradiente_y = conv2(double(img), sobel_y, 'same');
 
 % Calcolo del gradiente totale utilizzando la magnitudine del gradiente
-gradiente_totale = sqrt(gradiente_x.^2 + gradiente_y.^2);
+gradiente_magnitudine = sqrt(gradiente_x.^2 + gradiente_y.^2);
 
 
 % Calcolo dell'angolo del gradiente
@@ -41,7 +41,32 @@ gradiente_totale = sqrt(gradiente_x.^2 + gradiente_y.^2);
 % Visualizzazione dell'immagine originale e del gradiente magnitudine
 figure;
 subplot(1, 2, 1), imshow(img), title('Immagine originale');
-subplot(1, 2, 2), imshow(gradiente_totale, []), title('Gradiente Magnitudine');
+subplot(1, 2, 2), imshow(gradiente_magnitudine, []), title('Gradiente Magnitudine');
+
+% Trova i minimi locali nell'immagine del gradiente
+marcatori_interni = imregionalmin(gradiente_magnitudine);
+
+% Trova i marcatori esterni
+distanza_trasformata = bwdist(marcatori_interni);
+marcatori_esterni = imextendedmin(distanza_trasformata, 0.8);
+
+% Unisci i marcatori interni ed esterni
+marcatori = imimposemin(gradiente_magnitudine, marcatori_interni | marcatori_esterni);
+
+% Calcola la segmentazione watershed
+segmentazione = watershed(marcatori, 8);
+
+% Visualizza l'immagine originale e la segmentazione
+figure;
+subplot(1, 2, 1), imshow(img_resized), title('Immagine originale');
+subplot(1, 2, 2), imshow(label2rgb(segmentazione)), title('Segmentazione Watershed');
+
+% Calcola l'area del tumore utilizzando l'analisi dei componenti connessi
+regioni = bwlabel(segmentazione);
+tumore_area = max(histcounts(regioni, 'BinMethod', 'integers'));
+
+% Visualizza l'area del tumore
+disp(['L''area del tumore è: ', num2str(tumore_area), ' pixel']);
 
 imshow(gradiente_totale);
 % Identificazione dei marcatori (ad esempio, usando la ricerca dei massimi locali)
