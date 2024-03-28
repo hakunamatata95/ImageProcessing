7df60d584c2cc3139f9dc0b754008101722fc912
eltuzzador <spiderhp99@gmail.com>
2024-03-28 01:31:01 +0100

    rotation to mri

--- MriImport.m
+++ MriImport.m
@@ -1,8 +1,20 @@
+clear all;
+close all;
+
 label = niftiread('Task06_Lung\labelsTr\lung_095.nii.gz');
 depth = Helpers.search_max_region_in_label(label);
 
 mri = niftiread('Task06_Lung\imagesTr\lung_095.nii.gz');
+
 img = Helpers.extractyimage(mri, depth);
  
-imshow(img);
-imshow(imageFromMRI, [], 'InitialMagnification', 'fit');
+imshow(imrotate(img, 90),[]);
+
+%volshow(mri);
+
+%normalizzazione min max
+%Vmax= max(mri, [], 'all');
+%Vmin= min(mri, [], 'all');
+%model = (mri-Vmin)./(Vmax-Vmin);
+
+%imshow(imageFromMRI, [], 'InitialMagnification', 'fit');
