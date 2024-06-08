ecdaecbf676ab364a24a0b86fa09721e02da2bac
eltuzzador <spiderhp99@gmail.com>
2024-06-08 18:54:55 +0200

    comments refactor

--- Helpers.m
+++ Helpers.m
@@ -47,9 +47,9 @@ classdef Helpers
              fileNames = {listaFiles.name};
         end
 
-        % Utilizzando i percorsi delle mri relative ad un training ed una
-        % label trova le immagini con la dimensione del tumore massima, la
-        % estrae e la normalizza, restituendola come output
+        % Utilizzando i percorsi delle mri relative al training ed alla
+        % label trova le immagini con la dimensione del tumore massima: la
+        % estrae, la normalizza (range 0-1) e la restituisce come output
         function [imageTraining, imageLabel] = calculate_images_for_training_and_label(imagePath, labelPath)
             % Calcoliamo la nuova proporzione in base ai dati niftiread
             label = niftiread(labelPath);
@@ -67,25 +67,26 @@ classdef Helpers
             imageLabel = imrotate(Helpers.mriproportionadjustement(imageLabel, infoLabel), 90);
         end
         
-        % Corregge la proporzione di un immagine
+        % Corregge la proporzione dell'immagine importata adattandola alla
+        % pixel dimension degli assi di interesse.
+        % Input: immagine e info niftiread .nii.gz
         function image_corrected = mriproportionadjustement(image, info)
             x = info.ImageSize(1) * info.PixelDimensions(1);
             y = info.ImageSize(3) * info.PixelDimensions(3);
             new_size = [x, y];
 
-            image_corrected = imresize(image,new_size);
+            image_corrected = imresize(image, new_size);
         end
 
-        % Prorziona l'intensità dei pixel in un range tra 0 e 1
+        % Proporziona l'intensità dei pixel in un range tra 0 e 1
         function img_normalized = imagenormalize(image)
             min_val = min(image(:)); % Trova il valore minimo dei pixel
             max_val = max(image(:)); % Trova il valore massimo dei pixel
             img_normalized = (double(image) - min_val) / (max_val - min_val); % Normalizza l'immagine
         end
 
-        % Effettua il resize dell'immagine basandosi sulla larghezza presa
-        % in input e mantenendo la proporzione tra altezza e larghezza
-        % invariata
+        % Ritorna una immagine proporzionata in altezza rispetto alla
+        % larghezza obiettivo fornita
         function imageResized = resize(img, newWidth)
             % Calcoliamo la nuova altezza proporzionale b:h=B:H
             originalSize = size(img);
@@ -96,8 +97,8 @@ classdef Helpers
             imageResized = imresize(img, new_size);
         end
  
-        % Trasforma l'immagine in scala di grigi se necessario e
-        % utilizzando il metodo graythresh calcola la soglia che poi
+        % Trasforma l'immagine in scala di grigi (se necessario) e,
+        % utilizzando il metodo graythresh, calcola la soglia che poi
         % utilizza per binarizzare l'immagine
         function binImage = otsubin(image)
             image = Helpers.rgb2gray(image);
@@ -105,9 +106,8 @@ classdef Helpers
             binImage = imbinarize(image, greyLevel);
         end
 
-        % Data una mri di una label, trova l'immagine in essa contenuta con
-        % l'estensione del tumore maggiore, restituisce l'immagine con la
-        % maggior estensione tumorale e la sua profondità
+        % Data una MRI di una label trova il frame con l'area maggiore del tumore
+        % Output: l'immagine con la maggior estensione tumorale e la sua profondità
         function [image, depth] = search_max_region_in_label(mri)
             depth = 0;
             maxArea = 0;
@@ -128,8 +128,8 @@ classdef Helpers
 
         end 
 
-        % Data una mri ed una profondità estrae la relativa immagine posta
-        % sul piano orizzontale e la restituisce in output
+        % Data una MRI ed una profondità estrae il relativo frame posto
+        % sul piano orizzontale restituendolo in output
         function imageFromMRI = extractyimage(mri, depth)
             if depth < -size(mri, 2) || depth > size(mri, 2)
                 error('Depth is wrong'); 
@@ -159,7 +159,7 @@ classdef Helpers
         end  
 
 
-        % Mostra le immagini prese in input con i relativi nomi
+        % Mostra n immagini prese in input con i relativi nomi (opzionali)
         function imsshow(arrayImgs, namesImgs)
             numImgs = numel(arrayImgs);
             
@@ -194,7 +194,7 @@ classdef Helpers
         end
 
 
-        % Converte un'immagine in scala di grigi se necessario
+        % Converte un'immagine in scala di grigi (se necessario)
         function img = rgb2gray(img)
             if size(img, 3) == 3
                 img = rgb2gray(img);
