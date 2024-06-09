b6eab16b71a1ed66dc4aac8c5519c98e31cccd61
simone ciavarella <ciavarellasimonecarm@tiscali.it>
2024-06-09 16:11:27 +0200

    Modified Evaluation class

--- Evaluation.m
+++ Evaluation.m
@@ -6,6 +6,15 @@ examplesFolders = Helpers.elenca_file_con_prefisso('Dataset', 'lung');
 
 newSize = [128, 128];
 
+TPThreshold = 0;
+TNThreshold = 0;
+FPThreshold = 0;
+FNThreshold = 0;
+
+TPMarkerControlled = 0;
+TNMarkerControlled = 0;
+FPMarkerControlled = 0;
+FNMarkerControlled = 0;
 
 for j = 1 : size(examplesFolders,2)
     
@@ -18,6 +27,34 @@ for j = 1 : size(examplesFolders,2)
     markerControlledOccurences = imresize(markerControlledOccurences, newSize);
     labels = imresize(labels, newSize);
 
-    [TP, TN, FP, FN, ACC, TPR, TNR] = Helpers.calculate_metrics(labels, thresholdOccurences);
+    [TP, TN, FP, FN] = Helpers.calculate_metrics(labels, thresholdOccurences);
 
+    TPThreshold = TPThreshold + TP;
+    TNThreshold = TNThreshold + TN;
+    FPThreshold = FPThreshold + FP;
+    FNThreshold = FNThreshold + FN;
+
+    [TPa, TNa, FPa, FNa] = Helpers.calculate_metrics(labels, markerControlledOccurences);
+
+    TPMarkerControlled = TPMarkerControlled + TPa;
+    TNMarkerControlled = TNMarkerControlled + TNa;
+    FPMarkerControlled = FPMarkerControlled + FPa;
+    FNMarkerControlled = FNMarkerControlled + FNa;
 end
+
+ACCThreshold = (TPThreshold + TNThreshold) / (TPThreshold + TNThreshold + FPThreshold + FNThreshold);
+TPRThreshold = TPThreshold / (TPThreshold + FNThreshold);
+TNRThreshold = TNThreshold / (TNThreshold + FPThreshold);
+
+ACCMarkerControlled = (TPMarkerControlled + TNMarkerControlled) / (TPMarkerControlled + TNMarkerControlled + FPMarkerControlled + FNMarkerControlled);
+TPRMarkerControlled = TPMarkerControlled / (TPMarkerControlled + FNMarkerControlled);
+TNRMarkerControlled = TNMarkerControlled / (TNMarkerControlled + FPMarkerControlled);
+
+% Stampa dei risultati
+fprintf('ACCThreshold = %.4f\n', ACCThreshold);
+fprintf('TPRThreshold = %.4f\n', TPRThreshold);
+fprintf('TNRThreshold = %.4f\n', TNRThreshold);
+
+fprintf('ACCMarkerControlled = %.4f\n', ACCMarkerControlled);
+fprintf('TPRMarkerControlled = %.4f\n', TPRMarkerControlled);
+fprintf('TNRMarkerControlled = %.4f\n', TNRMarkerControlled);
